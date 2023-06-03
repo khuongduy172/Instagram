481045eb9786062cfe479c4f06ad9ff3275cc16f
Duy Nguyen <nguyenngockhuongduy172@gmail.com>
2023-06-03 11:03:54 +0700

    hide onboarding screen when open app and logged in

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -30,15 +30,24 @@ import {
   CommentScreen,
 } from '../screens';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { getUserOwner } from '../apis/userApi';
+import { ActivityIndicator } from 'react-native-paper';
 
 const AppNavigation = () => {
   const Stack = createNativeStackNavigator();
   const isLoggedIn = useSelector((state: any) => state.auth.isLoggedIn);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   const checkLoginStatus = async () => {
     const token = await AsyncStorage.getItem('accessToken');
     if (token) {
-      store.dispatch(setLoggedIn(true));
+      let user = await getUserOwner();
+      if (user) {
+        store.dispatch(setLoggedIn(true));
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -52,33 +61,37 @@ const AppNavigation = () => {
         barStyle={scheme === 'dark' ? 'light-content' : 'dark-content'}
         backgroundColor={scheme === 'dark' ? '#000' : '#fff'}
       />
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isLoggedIn ? (
-          <>
-            <Stack.Screen name="TabNavigation" component={TabNavigation} />
-            <Stack.Screen name="StoryScreen" component={StoryScreen} />
-            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-            <Stack.Screen name="Setting" component={SettingScreen} />
-            <Stack.Screen name="EditImage" component={EditImageScreen} />
-            <Stack.Screen name="NewPost" component={NewPost} />
-            <Stack.Screen name="Direct" component={DirectScreen} />
-            <Stack.Screen name="Message" component={MessageScreen} />
-            <Stack.Screen name="CreateReels" component={CreateReels} />
-            <Stack.Screen name="PostReels" component={PostReels} />
-            <Stack.Screen name="Editor" component={EditorScreen} />
-            <Stack.Screen name="Activity" component={ActivityScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="ClientProfile" component={ClientProfile} />
-            <Stack.Screen name="Comment" component={CommentScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
-          </>
-        )}
-      </Stack.Navigator>
+      {isLoading ? (
+        <ActivityIndicator style={{}} size={'large'} />
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {isLoggedIn ? (
+            <>
+              <Stack.Screen name="TabNavigation" component={TabNavigation} />
+              <Stack.Screen name="StoryScreen" component={StoryScreen} />
+              <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+              <Stack.Screen name="Setting" component={SettingScreen} />
+              <Stack.Screen name="EditImage" component={EditImageScreen} />
+              <Stack.Screen name="NewPost" component={NewPost} />
+              <Stack.Screen name="Direct" component={DirectScreen} />
+              <Stack.Screen name="Message" component={MessageScreen} />
+              <Stack.Screen name="CreateReels" component={CreateReels} />
+              <Stack.Screen name="PostReels" component={PostReels} />
+              <Stack.Screen name="Editor" component={EditorScreen} />
+              <Stack.Screen name="Activity" component={ActivityScreen} />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+              <Stack.Screen name="ClientProfile" component={ClientProfile} />
+              <Stack.Screen name="Comment" component={CommentScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="SignUp" component={SignUpScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
