c324ca0c5948fba8000ab9050a0888f8f09fd46d
Duy Nguyen <nguyenngockhuongduy172@gmail.com>
2023-06-07 16:33:31 +0700

    done set up push notifications

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+});
 
 AppRegistry.registerComponent(appName, () => App);
